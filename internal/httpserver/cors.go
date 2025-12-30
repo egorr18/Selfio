cbc09ec2ba2d6b619af6bb4345745176bbc168c9
Egor Korol <egorkakorol2006@gmail.com>
2025-12-30 10:58:38 +0200

    fix(cors): allow X-Admin-Key header

--- internal/httpserver/cors.go
+++ internal/httpserver/cors.go
@@ -21,7 +21,7 @@ func corsMiddleware(next http.Handler) http.Handler {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 			w.Header().Set("Vary", "Origin")
 			w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Admin-Key")
 		}
 
 		// ВАЖЛИВО: відповідь на preflight (OPTIONS)
